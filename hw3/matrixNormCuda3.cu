602828194cb4858fbdca2d1eb3f68cb06743aead
Guillermo <g.puente.allott@gmail.com>
2014-03-10 13:20:12 -0500

    HW3

--- hw3/matrixNormCuda3.cu
+++ hw3/matrixNormCuda3.cu
@@ -216,13 +216,13 @@ partialSum(float *input, float *output, const int N, const int Nsums) {
 
     // If we are inside the input array, we transfer the value that we're going to sum up to the partial sum array
     if (start + t < N)
-      partialSum[t + ty*2*BLOCK_SIZE] = input[blockIdx.y*2*MAXN + ty*NMAX + x];
+      partialSum[t + ty*2*BLOCK_SIZE] = input[blockIdx.y*2*MAXN + ty*MAXN + x];
     else
       partialSum[t + ty*2*BLOCK_SIZE] = 0;
    
     // The same for the last element of the block, the other value that we're going to sum up
     if (start + BLOCK_SIZE + t < N)
-      partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = input[blockIdx.y*2*MAXN + BLOCK_SIZE*NMAX + ty*NMAX + x];
+      partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = input[blockIdx.y*2*MAXN + BLOCK_SIZE*MAXN + ty*NMAX + x];
     else
       partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = 0; //
    
