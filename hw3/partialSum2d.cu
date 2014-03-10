4d835e352476f4adab78d1b9f4d95e4492a676b1
Guillermo <g.puente.allott@gmail.com>
2014-03-09 21:42:05 -0500

    HW3

--- hw3/partialSum2d.cu
+++ hw3/partialSum2d.cu
@@ -19,7 +19,7 @@ partialSum(float *input, float *output, const int N, const int Noutput) {
 
 	// Load a segment of the input vector into shared memory
 	// This is because the entire array might be too big and is stored into the global memory
-    __shared__ float partialSum[2 * BLOCK_SIZE*BLOCK_SIZE];
+    __shared__ float partialSum[4 * BLOCK_SIZE*BLOCK_SIZE];
 
     // Position in the input array
     unsigned int t = threadIdx.x;
