884287282e20c3cf7d45870a7470a05b79dbbfea
Guillermo <g.puente.allott@gmail.com>
2014-03-09 21:13:27 -0500

    HW3

--- hw3/matrixNormCuda.cu
+++ hw3/matrixNormCuda.cu
@@ -182,7 +182,7 @@ partialSum(float *input, float *output, const int N, const int Nmeans) {
 
   // Load a segment of the input vector into shared memory
   // This is because the entire array might be too big and is stored into the global memory
-    __shared__ float partialSum[2 * BLOCK_SIZE * N];
+    __shared__ float partialSum[ N * 2 * BLOCK_SIZE];
 
     // Position in the input array
     unsigned int t = threadIdx.x;
