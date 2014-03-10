ed5782dda3f5f8eb02d56b5e67461eec0bd5ad8e
Guillermo <g.puente.allott@gmail.com>
2014-03-10 11:25:12 -0500

    HW3

--- hw3/matrixNormCuda3.cu
+++ hw3/matrixNormCuda3.cu
@@ -221,7 +221,7 @@ void matrixNorm() {
   float *d_sums, *d_A, *d_B;
 
   //Get user input into size;
-  float (*h_sums)[N] = new float[BLOCK_SIZE][N];
+  float (*h_sums)[N] = new float[N][N];
 
   for (int i=0; i < BLOCK_SIZE; i++)
       for (int j=0; j < N; j++)
