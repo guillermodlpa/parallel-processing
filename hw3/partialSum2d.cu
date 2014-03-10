d25afabfc7b152d1b8ea1cb4a9f3ffb62d9dd2bf
Guillermo <g.puente.allott@gmail.com>
2014-03-10 01:15:18 -0500

    HW3

--- hw3/partialSum2d.cu
+++ hw3/partialSum2d.cu
@@ -72,7 +72,7 @@ partialSum(float *input, float *output, const int N, const int Noutput) {
     // So we have to put it in the output array
     if (t == 0)
        //output[blockIdx.x + y*Noutput] += partialSum[0+ty*BLOCK_SIZE];
-      output[blockIdx.x + y*Noutput] = partialSum[ty*2*BLOCK_SIZE];
+      output[blockIdx.y + x*Noutput] = partialSum[ty*2*BLOCK_SIZE];
 }
 
 
