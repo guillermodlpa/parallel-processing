31b09edee7499ad0a7ea9ed85c4bec2475c3ce4b
Guillermo <g.puente.allott@gmail.com>
2014-03-09 20:07:08 -0500

    HW3

--- hw3/partialSum.cu
+++ hw3/partialSum.cu
@@ -23,6 +23,29 @@ partialSum(float *partialSum, const int N) {
 	}
 }
 
+__global__ void total(float * input, float * output, int len) {
+    //@@ Load a segment of the input vector into shared memory
+    __shared__ float partialSum[2 * BLOCK_SIZE];
+    unsigned int t = threadIdx.x, start = 2 * blockIdx.x * BLOCK_SIZE;
+    if (start + t < len)
+       partialSum[t] = input[start + t];
+    else
+       partialSum[t] = 0;
+    if (start + BLOCK_SIZE + t < len)
+       partialSum[BLOCK_SIZE + t] = input[start + BLOCK_SIZE + t];
+    else
+       partialSum[BLOCK_SIZE + t] = 0;
+    //@@ Traverse the reduction tree
+    for (unsigned int stride = BLOCK_SIZE; stride >= 1; stride >>= 1) {
+       __syncthreads();
+       if (t < stride)
+          partialSum[t] += partialSum[t+stride];
+    }
+    //@@ Write the computed sum of the block to the output vector at the 
+    //@@ correct index
+    if (t == 0)
+       output[blockIdx.x] = partialSum[0];
+}
 
 
 int
@@ -31,12 +54,13 @@ main()
 	int N = 17;
 	int num_bytes = N*sizeof(float);
 
-	float *d_a, *h_a;
+	float *d_a, *h_a, *h_o, *d_o;
 
 	h_a = (float*)malloc(num_bytes);
+	h_o = (float*)malloc(num_bytes);
 
 	for (int i=0; i < N; i++){   
-	    h_a[i]=0;
+	    h_a[i]=0; h_o[i]=0;
 	}
 
 	h_a[0]=1; 
@@ -64,23 +88,26 @@ main()
     } 
 
 	cudaMalloc( (void**)&d_a, num_bytes );
+	cudaMalloc( (void**)&d_o, num_bytes );
 	cudaMemcpy( d_a, h_a, num_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy( d_o, h_o, num_bytes, cudaMemcpyHostToDevice);
 
 	int blocksize = 8;
 	dim3 dimBlock( blocksize, 1 );
 	dim3 dimGrid( ceil(  ((float)N)/blocksize), 1 );
 
-	cout << "Size of grid " << ceil( N/blocksize) << endl;
-
-	partialSum<<< dimGrid, blocksize>>> (d_a, N);
+	partialSum<<< dimGrid, blocksize>>> (d_a, d_o N);
 
 	cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
+	cudaMemcpy( h_o, d_o, num_bytes, cudaMemcpyDeviceToHost );
 
 	cudaFree(d_a);
+	cudaFree(d_o);
 
 	printf("MATRIX AFTER\n\t");
 	for (i = 0; i < N; i++) {
-      cout << "h_a[" << i << "]=" << h_a[i] << endl;
+      cout << "h_o[" << i << "]=" << h_o[i] << endl;
     } 
     free(h_a);
+    free(h_o);
 }
