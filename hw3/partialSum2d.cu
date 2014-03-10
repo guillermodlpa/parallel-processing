39d6212bbbd27e4109cc0a8046bf6af1f47e1751
Guillermo <g.puente.allott@gmail.com>
2014-03-10 10:52:15 -0500

    HW3

--- hw3/partialSum2d.cu
+++ hw3/partialSum2d.cu
@@ -31,7 +31,7 @@ partialSum(float *input, float *output, const int N, const int Noutput) {
     __shared__ float partialSum[2* BLOCK_SIZE*BLOCK_SIZE];
 
     // Position in the input array
-    unsigned int t = threadIdx.y;
+    unsigned int t = threadIdx.x;
     unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
     unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
     unsigned int ty = threadIdx.y;
