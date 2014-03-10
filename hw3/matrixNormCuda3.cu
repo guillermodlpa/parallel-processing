9715a524685ab2df658b30181d227e26e8d5370d
Guillermo <g.puente.allott@gmail.com>
2014-03-10 13:23:12 -0500

    HW3

--- hw3/matrixNormCuda3.cu
+++ hw3/matrixNormCuda3.cu
@@ -216,20 +216,21 @@ partialSum(float *input, float *output, const int N, const int Nsums) {
 
     // If we are inside the input array, we transfer the value that we're going to sum up to the partial sum array
     if (start + t < N)
-      partialSum[t + ty*2*BLOCK_SIZE] = input[blockIdx.y*2*MAXN + ty*MAXN + x];
+      partialSum[t + ty*2*BLOCK_SIZE] = input[start + t + y*MAXN];
     else
       partialSum[t + ty*2*BLOCK_SIZE] = 0;
    
     // The same for the last element of the block, the other value that we're going to sum up
     if (start + BLOCK_SIZE + t < N)
-      partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = input[blockIdx.y*2*MAXN + BLOCK_SIZE*MAXN + ty*MAXN + x];
+      partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = input[start + BLOCK_SIZE + t + y*MAXN];
     else
-      partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = 0; //
+      partialSum[BLOCK_SIZE + t + ty*2*BLOCK_SIZE] = 0;
    
     // Perform the partial sum
     for (unsigned int stride = BLOCK_SIZE; stride >= 1; stride >>= 1) {
        __syncthreads();
        if (t < stride)
+          //partialSum[t+ty*BLOCK_SIZE] += partialSum[t+stride+ty*BLOCK_SIZE];
           partialSum[t + ty*2*BLOCK_SIZE] += partialSum[t+stride + ty*2*BLOCK_SIZE];
     }
 
@@ -237,8 +238,7 @@ partialSum(float *input, float *output, const int N, const int Nsums) {
     // So we have to put it in the output array
     if (t == 0)
        //output[blockIdx.x + y*Noutput] += partialSum[0+ty*BLOCK_SIZE];
-      //output[blockIdx.x*N + y] = partialSum[ty*2*BLOCK_SIZE];
-      output[blockIdx.y*N + x] = partialSum[ty*2*BLOCK_SIZE];
+      output[blockIdx.x*N + y] = partialSum[ty*2*BLOCK_SIZE];
 }
 
 
