a5d866480180599978e7a95c2c585cff6d4ed39f
Guillermo <g.puente.allott@gmail.com>
2014-04-11 05:12:50 -0500

    HW4

--- hw4/problem2/gauss.c
+++ hw4/problem2/gauss.c
@@ -165,11 +165,11 @@ void gaussianElimination() {
     	if ( my_rank == SOURCE ) {
     		int i;
 	    	for ( i = 1; i < p; i++ ) {
-	    		MPI_Send( A, 1, MPI_FLOAT, i,0, MPI_COMM_WORLD );
+	    		MPI_Send( &test, 1, MPI_FLOAT, i,0, MPI_COMM_WORLD );
 	    	}
 	    }
 	    else
-    		MPI_Recv( A, 1, MPI_FLOAT, SOURCE, 0, MPI_COMM_WORLD, &status);
+    		MPI_Recv( &test, 1, MPI_FLOAT, SOURCE, 0, MPI_COMM_WORLD, &status);
 
     	printf("Process number %d of %d says phase 1 completed\n",
             my_rank+1, p);
@@ -202,14 +202,14 @@ void gaussianElimination() {
             my_rank+1, p);
 
     	/* Now we collect the results */
-    	if ( my_rank != SOURCE )
-    		MPI_Send( A, N*N, MPI_FLOAT, SOURCE,0, MPI_COMM_WORLD );
+    	/*if ( my_rank != SOURCE )
+    		MPI_Send( &A, N*N, MPI_FLOAT, SOURCE,0, MPI_COMM_WORLD );
     	else {
     		int i;
     		for ( i = 1; i < p; i++ ) {
 	    		MPI_Recv( A, N*N, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
 	    	}
-	    }
+	    }*/
 
 	    printf("Process number %d of %d says phase 2 completed\n",
             my_rank+1, p);
