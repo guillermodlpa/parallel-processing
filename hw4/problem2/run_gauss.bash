ba633d6db434a04c47a628903d9eb6076af56678
Guillermo <g.puente.allott@gmail.com>
2014-04-11 01:27:11 -0500

    HW4

---
+++ hw4/problem2/run_gauss.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Modifying working directory
+# I did this to adapt the script to my custom working directory structure
+
+	cd parallel-processing/hw4/problem1
+	echo "Working directory is:"
+	pwd
+
+mpirun -npernode 8 ./gauss
