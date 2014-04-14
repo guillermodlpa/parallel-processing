78dd13c87eaebcea04da510697941d775314ccc5
Guillermo <g.puente.allott@gmail.com>
2014-04-14 18:56:10 -0500

    HW4

--- hw4/problem2/gauss.c
+++ hw4/problem2/gauss.c
@@ -241,7 +241,7 @@ void gauss() {
 		t2 = MPI_Wtime();
 		tick = MPI_Wtick(); // Time in seconds that an MPI tick represents
 
-		printf("\nElapsed time: %5.0f miliseconds\n", (t2-t1)*tick/1000);
+		printf("\nElapsed time: %5.3f miliseconds\n", (t2-t1)*tick/1000);
 	}
 	
 }
