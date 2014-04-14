0e82829cc751a6d67979f1cb0a35968934bec273
Guillermo <g.puente.allott@gmail.com>
2014-04-14 12:45:54 -0500

    HW4

--- hw4/problem2/gauss.c
+++ hw4/problem2/gauss.c
@@ -91,7 +91,9 @@ void gauss() {
 		int i;
 		float local_test [2];
 		for ( i = 1; i < p; i++ ) {
-    		MPI_Recv( &test, 2, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
+    		MPI_Recv( &local_test, 2, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
+    		test[0] = test[0] + local_test[0];
+    		test[1] = test[1] + local_test[1];
     	}
     	printf("\nProcess number %d of %d says: got %5.2f and %5.2f\n",
         my_rank+1, p, test[0], test[1]);
