e76cbdc53196d263b651043a14fc7a00d66799f9
Guillermo <g.puente.allott@gmail.com>
2014-04-14 12:50:57 -0500

    HW4

--- hw4/problem2/gauss.c
+++ hw4/problem2/gauss.c
@@ -34,8 +34,9 @@ int my_rank;
 /* The number of processes   */
 int p; 
 
+float *test;
 
-float test[2];
+#define N 4
 
 int main(int argc, char **argv) {
 
@@ -52,8 +53,7 @@ int main(int argc, char **argv) {
     printf("\nProcess number %d of %d says hi\n",
             my_rank+1, p);
 
-    test[0] = 0;
-    test[1] = 0;
+    test = (float*) malloc( N*sizeof(float));
     
     gauss();
 
@@ -66,9 +66,8 @@ void gauss() {
 	MPI_Status status;
 
     if ( my_rank == SOURCE ) {
-		int i;
-		test[0] = 1;
-		test[1] = 2;
+		int i,k;
+		for (k = 0; k < N; k++) test[k] = 1;
     	for ( i = 1; i < p; i++ ) {
     		MPI_Send( &test, 2, MPI_FLOAT, i,0, MPI_COMM_WORLD );
     	}
@@ -77,26 +76,26 @@ void gauss() {
 		MPI_Recv( &test, 2, MPI_FLOAT, SOURCE, 0, MPI_COMM_WORLD, &status);
 
 
-	test[0] = test[0] + 0.01f;
-	test[1] = test[1] + 0.02f;
+	int k;
+	for (k = 0; k < N; k++) test[k] += 0.01f;
 
 
-	printf("\nProcess number %d of %d says: got %5.2f and %5.2f\n",
-        my_rank+1, p, test[0], test[1]);
+	printf("\nProcess number %d of %d says: got %5.2f, %5.2f, %5.2f, %5.2f\n",
+        my_rank+1, p, test[0], test[1], test[2], test[3]);
 
 
 	if ( my_rank != SOURCE )
 		MPI_Send( &test, 2, MPI_FLOAT, SOURCE,0, MPI_COMM_WORLD );
 	else {
 		int i;
-		float local_test [2];
+		float *local_test = (float*) malloc( N*sizeof(float));
 		for ( i = 1; i < p; i++ ) {
     		MPI_Recv( &local_test, 2, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
-    		test[0] = test[0] + local_test[0];
-    		test[1] = test[1] + local_test[1];
+    		int k;
+			for (k = 0; k < N; k++) test[k] += local_test[k];
     	}
-    	printf("\nProcess number %d of %d says: got %5.2f and %5.2f\n",
-        my_rank+1, p, test[0], test[1]);
+    	printf("\nProcess number %d of %d says: got %5.2f, %5.2f, %5.2f, %5.2f\n",
+        	my_rank+1, p, test[0], test[1], test[2], test[3]);
     }
 
 
