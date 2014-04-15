a39c5a8fb29eb3a780b00dbcdca0a4e0fe97c1d1
Guillermo <g.puente.allott@gmail.com>
2014-04-15 18:01:25 -0500

    HW4

--- hw4/problem2/gauss.c
+++ hw4/problem2/gauss.c
@@ -3,8 +3,8 @@
 /* Guillermo de la Puente - A20314328                */
 /* CS 546 - Parallel and Distributed Processing      */
 /* Homework 4                                        */
-/* Part 1/2                                          */
-/* Modification of get_data.c                        */
+/* Part 2/2                                          */
+/* Gaussian elimination without pivoting with MPI    */
 /*                                                   */
 /* 2014 Spring semester                              */
 /* Professor Zhiling Lan                             */
@@ -12,358 +12,191 @@
 /* Illinois Institute of Technology                  */
 
 
-/* get_data.c -- Parallel Trapezoidal Rule, uses basic Get_data function for 
-
- *     input.
-
- *
-
- * Input: 
-
- *    a, b: limits of integration.
-
- *    n: number of trapezoids.
-
- * Output:  Estimate of the integral from a to b of f(x) 
-
- *    using the trapezoidal rule and n trapezoids.
-
- *
-
- * Notes:  
-
- *    1.  f(x) is hardwired.
-
- *    2.  Assumes number of processes (p) evenly divides
-
- *        number of trapezoids (n).
-
- *
-
- * See Chap. 4, pp. 60 & ff in PPMPI.
-
- */
-
 #include <stdio.h>
-
-
-/* We'll be using MPI routines, definitions, etc. */
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include <mpi.h>
 
-// PARAMETERS ///
+#define MAXN 4000 /* Max value of N */
+int N; /* Matrix size */
+#define DIVFACTOR 327680000.0f
 
-#define A 0
+#define SOURCE 0
 
-#define B 1
+/* My process rank           */
+int my_rank;
+/* The number of processes   */
+int p; 
 
-#define N 100
-
-/////////////////
-
-main(int argc, char** argv) {
-
-    int         my_rank;   /* My process rank           */
-
-    int         p;         /* The number of processes   */
-
-    float       a;         /* Left endpoint             */
-
-    float       b;         /* Right endpoint            */
-
-    int         n;         /* Number of trapezoids      */
-
-    float       h;         /* Trapezoid base length     */
-
-    float       local_a;   /* Left endpoint my process  */
-
-    float       local_b;   /* Right endpoint my process */
-
-    int         local_n;   /* Number of trapezoids for  */
-
-                           /* my calculation            */
-
-    float       integral;  /* Integral over my interval */
-
-    float       total;     /* Total integral            */
-
-    int         source;    /* Process sending integral  */
-
-    int         dest = 0;  /* All messages go to 0 initially, but we will change this variable      */
-
-    int         tag = 0;
-
-    MPI_Status  status;
+/* Matrixes given by a pointer */
+float *A, *B, *X;
 
 
-    void Get_data(float* a_ptr, float* b_ptr, 
 
-         int* n_ptr, int my_rank, int p);
+/* returns a seed for srand based on the time */
+unsigned int time_seed() {
+    struct timeval t;
+  struct timezone tzdummy;
 
-    float Trap(float local_a, float local_b, int local_n,
+  gettimeofday(&t, &tzdummy);
+  return (unsigned int)(t.tv_usec);
+}
 
-              float h);    /* Calculate local integral  */
+
+/* Set the program parameters from the command-line arguments */
+void parameters(int argc, char **argv) {
+  int seed = 0;  /* Random seed */
+  char uid[32]; /*User name */
+
+  /* Read command-line arguments */
+  srand(time_seed());  /* Randomize */
+
+  if (argc == 3) {
+    seed = atoi(argv[2]);
+    srand(seed);
+    if ( my_rank == SOURCE ) printf("Random seed = %i\n", seed);
+  } 
+  if (argc >= 2) {
+    N = atoi(argv[1]);
+    if (N < 1 || N > MAXN) {
+      if ( my_rank == SOURCE ) printf("N = %i is out of range.\n", N);
+      exit(0);
+    }
+  }
+  else {
+    if ( my_rank == SOURCE ) printf("Usage: %s <matrix_dimension> [random seed]\n",
+           argv[0]);    
+    exit(0);
+  }
+
+  /* Print parameters */
+  if ( my_rank == SOURCE )  printf("\nMatrix dimension N = %i.\n", N);
+}
 
 
-    /* Let the system do what it needs to start up MPI */
+/* Allocates memory for A, B and X */
+void allocate_memory() {
+	A = (float*)malloc( N*N*sizeof(float) );
+	B = (float*)malloc( N*sizeof(float) );
+	X = (float*)malloc( N*sizeof(float) );
+}
+
+/* Free allocated memory for arrays */
+void free_memory() {
+	free(A);
+	free(B);
+	free(X);
+}
+
+
+/* Print input matrices */
+void print_inputs() {
+  int row, col;
+
+  if (N < 10) {
+    printf("\nA =\n\t");
+    for (row = 0; row < N; row++) {
+      for (col = 0; col < N; col++) {
+	printf("%5.2f%s", A[col+N*row], (col < N-1) ? ", " : ";\n\t");
+      }
+    }
+    printf("\nB = [");
+    for (col = 0; col < N; col++) {
+      printf("%5.2f%s", B[col], (col < N-1) ? "; " : "]\n");
+    }
+  }
+}
+
+/* Print matrix A */
+void print_A() {
+  int row, col;
+
+  if (N < 10) {
+    printf("\nA =\n\t");
+    for (row = 0; row < N; row++) {
+      for (col = 0; col < N; col++) {
+	printf("%5.2f%s", A[col+N*row], (col < N-1) ? ", " : ";\n\t");
+      }
+    }
+  }
+}
+/* Print matrix B */
+void print_B() {
+  int col;
+  if (N < 10) {
+	  printf("\nB = [");
+	    for (col = 0; col < N; col++) {
+	      printf("%5.2f%s", B[col], (col < N-1) ? "; " : "]\n");
+	    }
+	}
+}
+/* Print matrix X */
+void print_X() {
+  int row;
+
+  if (N < 100) {
+    printf("\nX = [");
+    for (row = 0; row < N; row++) {
+      printf("%5.2f%s", X[row], (row < N-1) ? "; " : "]\n");
+    }
+  }
+}
+
+/* Initialize A and B (and X to 0.0s) */
+void initialize_inputs() {
+
+  int row, col;
+
+  printf("\nInitializing...\n");
+  for (row = 0; row < N; row++) {
+    for (col = 0; col < N; col++) {
+      A[col+N*row] = (float)rand() / DIVFACTOR;
+    }
+    B[row] = (float)rand() / DIVFACTOR;
+    X[row] = 0.0;
+  }
+
+}
+
+
+int main(int argc, char **argv) {
 
     MPI_Init(&argc, &argv);
 
     /* Get my process rank */
-
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
     /* Find out how many processes are being used */
-
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-    /* MODIFICATION */
-    /* We update the processor that handles the receiving messages to the one with highest rank */
-    dest = p-1;
+    /* Every process reads the parameters to prepare dimension */
+    //parameters(argc, argv);
 
-    Get_data(&a, &b, &n, my_rank, p);
+    /* Every process must allocate memory for the arrays */
+    //allocate_memory();
 
+    if ( my_rank == SOURCE ) {
+      printf("\nInitializing...\n");
 
-    h = (b-a)/n;    /* h is the same for all processes */
+        /* Initialize A and B */
+        //initialize_inputs();
 
-    local_n = n/p;  /* So is the number of trapezoids */
-
-
-    /* Length of each process' interval of 
-
-     * integration = local_n*h.  So my interval
-
-     * starts at: */
-
-    local_a = a + my_rank*local_n*h;
-
-    local_b = local_a + local_n*h;
-
-    integral = Trap(local_a, local_b, local_n, h);
-
-
-    /* Add up the integrals calculated by each process */ 
-
-    /* MODIFICATION */
-    /* It is now the process with highest rank the one that prints */
-    /* The hightest rank is calculated from 'p', the number of processes */
-    if (my_rank ==  p - 1) {
-
-        total = integral;
-
-        for (source = 0; source < p-1; source++) {
-
-            MPI_Recv(&integral, 1, MPI_FLOAT, source, tag, 
-
-                MPI_COMM_WORLD, &status);
-
-            total = total + integral;
-
-        }
-
-    } else {   
-
-        MPI_Send(&integral, 1, MPI_FLOAT, dest, 
-
-            tag, MPI_COMM_WORLD);
-
+        /* Print input matrices */
+        //print_inputs();
     }
 
+    /* Free memory used for the arrays that we allocated previously */
+    //free_memory();
 
-    /* Print the result */
-
-    /* MODIFICATION */
-    /* It is now the process with highest rank the one that prints */
-    /* The hightest rank is calculated from 'p', the number of processes */
-    if (my_rank == p - 1) {
-
-        printf("\nProcess number %d of %d says:\n",
-            my_rank+1, p);
-        printf("With n = %d trapezoids, our estimate\n", 
-
-            n);
-
-        printf("of the integral from %f to %f = %f\n\n", 
-
-            a, b, total); 
-
-    }
-
-
-    /* Shut down MPI */
+    /* The barrier prevents any process to reach the finalize before the others have finished their communications */
+    MPI_Barrier(MPI_COMM_WORLD);
 
     MPI_Finalize();
 
-} /*  main  */
-
-
-
-/********************************************************************/
-
-/* Function Get_data
-
- * Reads in the user input a, b, and n.
-
- * Input parameters:
-
- *     1.  int my_rank:  rank of current process.
-
- *     2.  int p:  number of processes.
-
- * Output parameters:  
-
- *     1.  float* a_ptr:  pointer to left endpoint a.
-
- *     2.  float* b_ptr:  pointer to right endpoint b.
-
- *     3.  int* n_ptr:  pointer to number of trapezoids.
-
- * Algorithm:
-
- *     1.  Process 0 prompts user for input and
-
- *         reads in the values.
-
- *     2.  Process 0 sends input values to other
-
- *         processes.
-
- */
-
-void Get_data(
-
-         float*  a_ptr    /* out */, 
-
-         float*  b_ptr    /* out */, 
-
-         int*    n_ptr    /* out */,
-
-         int     my_rank  /* in  */, 
-
-         int     p        /* in  */) {
-
-
-    int source = 0;    /* All local variables used by */
-
-    int dest;          /* MPI_Send and MPI_Recv       */
-
-    int tag;
-
-    MPI_Status status;
-
-
-    if (my_rank == 0){
-
-        *a_ptr = A;
-        *b_ptr = B;
-        *n_ptr = N;
-
-        for (dest = 1; dest < p; dest++){
-
-            tag = 0;
-
-            MPI_Send(a_ptr, 1, MPI_FLOAT, dest, tag, 
-
-                MPI_COMM_WORLD);
-
-            tag = 1;
-
-            MPI_Send(b_ptr, 1, MPI_FLOAT, dest, tag, 
-
-                MPI_COMM_WORLD);
-
-            tag = 2;
-
-            MPI_Send(n_ptr, 1, MPI_INT, dest, tag, 
-
-                MPI_COMM_WORLD);
-
-        }
-
-    } else {
-
-        tag = 0;
-
-        MPI_Recv(a_ptr, 1, MPI_FLOAT, source, tag, 
-
-            MPI_COMM_WORLD, &status);
-
-        tag = 1;
-
-        MPI_Recv(b_ptr, 1, MPI_FLOAT, source, tag, 
-
-            MPI_COMM_WORLD, &status);
-
-        tag = 2;
-
-        MPI_Recv(n_ptr, 1, MPI_INT, source, tag, 
-
-                MPI_COMM_WORLD, &status);
-
-    }
-
-} /* Get_data */
-
-
-
-/********************************************************************/
-
-float Trap(
-
-          float  local_a   /* in */, 
-
-          float  local_b   /* in */, 
-
-          int    local_n   /* in */, 
-
-          float  h         /* in */) { 
-
-
-    float integral;   /* Store result in integral  */ 
-
-    float x; 
-
-    int i; 
-
-
-    float f(float x); /* function we're integrating */
-
-
-    integral = (f(local_a) + f(local_b))/2.0; 
-
-    x = local_a; 
-
-    for (i = 1; i <= local_n-1; i++) { 
-
-        x = x + h; 
-
-        integral = integral + f(x); 
-
-    } 
-
-    integral = integral*h; 
-
-    return integral;
-
-} /*  Trap  */
-
-
-
-/********************************************************************/
-
-float f(float x) { 
-
-    float return_val; 
-
-    /* Calculate f(x). */
-
-    /* Store calculation in return_val. */ 
-
-    return_val = x*x;
-
-    return return_val; 
-
-} /* f */
-
-
-
+    exit(0);
+}
