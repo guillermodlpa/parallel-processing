5547c65008b9ecf54cddb3cf7ff2016dc967adbc
Guillermo <g.puente.allott@gmail.com>
2014-04-15 03:24:14 -0500

    HW4

--- hw4/problem2/gauss.c
+++ hw4/problem2/gauss.c
@@ -306,7 +306,7 @@ void gaussElimination() {
 		    	/* In case this process isn't assigned any task, continue. This happens when there are more processors than rows */
 		    	if ( number_of_rows_r > 0  && remote_row_a < N) {
 
-		    		MPI_Send( &A[remote_row_a * N], N * number_of_rows_r, MPI_FLOAT, i,0, MPI_COMM_WORLD );
+		    		//MPI_Send( &A[remote_row_a * N], N * number_of_rows_r, MPI_FLOAT, i,0, MPI_COMM_WORLD );
 		    		MPI_Send( &B[remote_row_a],         number_of_rows_r, MPI_FLOAT, i,0, MPI_COMM_WORLD );
 		    	}
 	    	}
@@ -316,7 +316,7 @@ void gaussElimination() {
 
     		if ( number_of_rows > 0  && local_row_a < N) {
 
-	    		MPI_Recv( &A[local_row_a * N], N * number_of_rows, MPI_FLOAT, SOURCE, 0, MPI_COMM_WORLD, &status);
+	    		//MPI_Recv( &A[local_row_a * N], N * number_of_rows, MPI_FLOAT, SOURCE, 0, MPI_COMM_WORLD, &status);
 	    		MPI_Recv( &B[local_row_a],         number_of_rows, MPI_FLOAT, SOURCE, 0, MPI_COMM_WORLD, &status);
 	    	}
     	}
@@ -399,7 +399,7 @@ void gaussElimination() {
 			/*printf("\nIteration number %d of %d\n",
 			        norm+1, N-1);
 	    	print_A();*/
-			free(A);
+		
     	}
 
     }
