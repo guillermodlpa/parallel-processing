f509cff49cf1e9a2a00b3ce3d13e8e98adbb1357
Guillermo <g.puente.allott@gmail.com>
2014-05-06 23:01:11 -0500

    Project

--- project/programs/project_3.c
+++ project/programs/project_3.c
@@ -41,8 +41,8 @@ int main (int argc, char **argv) {
    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
    /* Input files */
-   const char* filename1 = argc == 3 ? argv[1] : "sample/1_im1";
-   const char* filename2 = argc == 3 ? argv[2] : "sample/1_im2";
+   const char* filename1 = argc == 3 ? argv[1] : "sample/2_im1";
+   const char* filename2 = argc == 3 ? argv[2] : "sample/2_im2";
 
    if ( my_rank==0) printf("\nCS 546 Project: MPI with Send + Recv\n");
    if ( my_rank==0) printf("CS 546 Project: Number of processors = %d\n",p);
