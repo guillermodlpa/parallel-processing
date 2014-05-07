50a8d36fdc2105317dcaf3d34c3c7d00295acc6c
Guillermo <g.puente.allott@gmail.com>
2014-05-07 00:17:43 -0500

    Project

--- project/programs/project_3.c
+++ project/programs/project_3.c
@@ -99,9 +99,9 @@ int main (int argc, char **argv) {
 
 /*-------------------------------------------------------------------------------------------------------*/
    /* Apply 1D FFT in all rows of A and B */
-   #pragma omp parallel num_threads(NUM_THREADS) shared(A,B)
+   //#pragma omp parallel num_threads(NUM_THREADS) shared(A,B)
    {
-      #pragma omp for private(i)
+      //#pragma omp for private(i)
       for (i= chunk*my_rank ;i< chunk*(my_rank+1);i++) {
             c_fft1d(A[i], N, -1);
             c_fft1d(B[i], N, -1);
