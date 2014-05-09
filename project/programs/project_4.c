f9429b939828976b21097359df56877ae0163dcf
Guillermo <g.puente.allott@gmail.com>
2014-05-09 01:40:39 -0500

    Project

--- project/programs/project_4.c
+++ project/programs/project_4.c
@@ -120,13 +120,13 @@ int main (int argc, char **argv) {
       MPI_Group_incl(world_group, p/4, P1_array, &P1);
       MPI_Comm_create( MPI_COMM_WORLD, P1, &P1_comm);
       MPI_Group_rank(P1, &my_grp_rank);
-      MPI_Intercomm_create(P1, 0, P2, 0, 111, P1_P2_inter);
+      MPI_Intercomm_create(P1_comm, 0, P2_comm, 0, 111, P1_P2_inter);
    } 
    else if ( processor_group == 1 ) { 
       MPI_Group_incl(world_group, p/4, P2_array, &P2); 
       MPI_Comm_create( MPI_COMM_WORLD, P2, &P2_comm);
       MPI_Group_rank(P2, &my_grp_rank);
-      MPI_Intercomm_create(P2, 0, P1, 0, 111, P1_P2_inter);
+      MPI_Intercomm_create(P2_comm, 0, P1_comm, 0, 111, P1_P2_inter);
    } 
    else if ( processor_group == 2 ) { 
       MPI_Group_incl(world_group, p/4, P3_array, &P3); 
